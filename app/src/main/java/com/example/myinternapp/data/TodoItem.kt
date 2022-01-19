b6c781a9475c7535b5f88da6ea7052d402cc5780
akab1902 <akab2310@gmail.com>
2022-01-19 18:25:23 +0800

    Edited TodoItem data class

--- app/src/main/java/com/example/myinternapp/data/TodoItem.kt
+++ app/src/main/java/com/example/myinternapp/data/TodoItem.kt
@@ -1,13 +1,22 @@
 package com.example.myinternapp.data
 
+import android.os.Parcelable
+import androidx.room.Entity
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
+@Entity (tableName = "todoItems_table")
+@Parcelize
 data class TodoItem(
    val name: String,
    val description: String,
    val tags: ArrayList<Tag>,
-   val completed: Boolean,
-   val createdDate: Date,
+   val completed: Boolean = false,
+   val createdTime: Long = System.currentTimeMillis(),
    val deadlineDate: Date
-)
+): Parcelable {
+   val createdDate: String
+      get() = DateFormat.getDateTimeInstance().format(createdTime)
+}
